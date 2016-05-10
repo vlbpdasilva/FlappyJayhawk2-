bc4dc89412063e5c6620cb731a45c25ee16e4ead
j574y923 <j574y923@ku.edu>
2016-05-10 17:08:03 -0500

    Update docs for bullet_time

--- PowerUps/bullet_time.py
+++ PowerUps/bullet_time.py
@@ -2,6 +2,8 @@ import pygame,os
 import settings
 from PowerUp import *
 class bullet_time(PowerUp):
+    """When you take the red pill
+    """
     SCREEN = pygame.display.set_mode((600,500))
     def __init__(self):
         super(bullet_time, self).__init__((0,255,0), (0,0), 20, 0,
@@ -9,7 +11,9 @@ class bullet_time(PowerUp):
                                                'bullet_time')
 
     def effect(self):
-        """Press and hold up to slow down time"""
+        """Press and hold up to slow down time
+        Release up to speed time back to normal
+        """
         #print 'MATRIX EXISTS'
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
@@ -22,6 +26,8 @@ class bullet_time(PowerUp):
                 settings.FPS = 60
 
     def effect_expire(self):
+        """Time goes back to normal
+        """
         settings.FPS += 3
         if(settings.FPS > 60):
             settings.FPS = 60
